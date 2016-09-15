1550cb284c281a61d3e63fae48f55e8c0bc4efa9
Carlos Becerra <carlos.becerra1234@gmail.com>
2016-09-15 18:22:36 -0300

    First commit

---
+++ src/main/java/com/wedevol/util/BackOffStrategy.java
@@ -0,0 +1,71 @@
+package com.wedevol.util;
+
+import java.util.Random;
+
+/**
+ * Util class for back off strategy
+ */
+
+public class BackOffStrategy {
+
+	public static final int DEFAULT_RETRIES = 5;
+	public static final long DEFAULT_WAIT_TIME_IN_MILLI = 1000;
+
+	private int numberOfRetries;
+	private int numberOfTriesLeft;
+	private long defaultTimeToWait;
+	private long timeToWait;
+	private Random random = new Random();
+
+	public BackOffStrategy() {
+		this(DEFAULT_RETRIES, DEFAULT_WAIT_TIME_IN_MILLI);
+	}
+
+	public BackOffStrategy(int numberOfRetries, long defaultTimeToWait) {
+		this.numberOfRetries = numberOfRetries;
+		this.numberOfTriesLeft = numberOfRetries;
+		this.defaultTimeToWait = defaultTimeToWait;
+		this.timeToWait = defaultTimeToWait;
+	}
+
+	/**
+	 * @return true if there are tries left
+	 */
+	public boolean shouldRetry() {
+		return numberOfTriesLeft > 0;
+	}
+
+	public void errorOccured() throws Exception {
+		numberOfTriesLeft--;
+		if (!shouldRetry()) {
+			throw new Exception("Retry Failed: Total of attempts: " + numberOfRetries + ". Total waited time: "
+					+ timeToWait + "ms.");
+		}
+		waitUntilNextTry();
+		timeToWait *= 2;
+		// we add a random time (google recommendation)
+		timeToWait += random.nextInt(500);
+	}
+
+	private void waitUntilNextTry() {
+		try {
+			Thread.sleep(timeToWait);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public long getTimeToWait() {
+		return this.timeToWait;
+	}
+
+	/**
+	 * Reset back off state. Call this method after successful attempts if you
+	 * want to reuse the class.
+	 */
+	public void reset() {
+		this.numberOfTriesLeft = numberOfRetries;
+		this.timeToWait = defaultTimeToWait;
+	}
+
+}
---
+++ src/test/java/com/wedevol/util/BackOffStrategyTest.java
@@ -0,0 +1,115 @@
+package com.wedevol.util;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.*;
+
+import com.wedevol.util.BackOffStrategy;
+
+/**
+ * Test for the backoff strategy
+ */
+
+public class BackOffStrategyTest {
+
+	private BackOffStrategy backoff;
+	private BackEndSimulator backend;
+
+	// Run once, e.g. Database connection, connection pool
+	@BeforeClass
+	public static void runOnceBeforeClass() {
+	}
+
+	// Run once, e.g close connection, cleanup
+	@AfterClass
+	public static void runOnceAfterClass() {
+	}
+
+	// e.g. Creating a similar object and share for all @Test
+	@Before
+	public void runBeforeTestMethod() {
+		backoff = new BackOffStrategy();
+		backend = new BackEndSimulator();
+	}
+
+	// Should rename to @AfterTestMethod
+	@After
+	public void runAfterTestMethod() {
+	}
+
+	@Test
+	public void testSuccessfulRegistration() throws Exception {
+		Boolean flag = backend.successfulRegistrationToDataBase();
+		assertEquals("Successful call", true, flag);
+	}
+
+	@Test
+	public void testMaximumUnsuccessfulCalls() {
+		int counter = 0;
+		while (backoff.shouldRetry()) {
+			try {
+				counter++;
+				backend.unsuccessfulRegistrationToDataBase();
+			} catch (Exception e) {
+				try {
+					backoff.errorOccured();
+				} catch (Exception e2) {
+				}
+			}
+		}
+		assertEquals("Unsuccessful calls", 5, counter);
+	}
+
+	@Test
+	public void testTimeIncreaseDuringUnsuccessfulCalls() {
+		backoff.reset();
+		long prev = backoff.getTimeToWait();
+		assertEquals("Default time is 1 seg", 1000, prev);
+		while (backoff.shouldRetry()) {
+			try {
+				backend.unsuccessfulRegistrationToDataBase();
+			} catch (Exception e) {
+				try {
+					backoff.errorOccured();
+					assertTrue("Time is increasing exponentially", backoff.getTimeToWait() > prev);
+				} catch (Exception e2) {
+				}
+			}
+		}
+	}
+
+	@Test(expected = Exception.class)
+	public void testExceptionIsThrownWhenReachedMaximumUnsuccessfulCalls() throws Exception {
+		backoff.reset();
+		while (backoff.shouldRetry()) {
+			try {
+				backend.unsuccessfulRegistrationToDataBase();
+			} catch (Exception e) {
+				try {
+					backoff.errorOccured();
+				} catch (Exception e2) {
+					System.out.println(e2);
+					throw e2;
+				}
+			}
+		}
+	}
+
+}
+
+/**
+ * Back off simulator class used for testing purposes
+ */
+
+class BackEndSimulator {
+
+	public boolean successfulRegistrationToDataBase() throws Exception {
+		return true;
+	}
+
+	public boolean unsuccessfulRegistrationToDataBase() throws Exception {
+		throw new Exception("Something occured in the database");
+	}
+
+}
